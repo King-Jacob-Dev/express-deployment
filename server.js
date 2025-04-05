a3e7964f9ce38b9947629c7e089002ac0d2e4b2b
King <eyadahmed998877@gmail.com>
2025-04-05 06:12:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 9080;
 
 app.get("/",(req,res) => {
-  res.send("Hello World");
+  res.send(`Hello World!\nVar = ${process.env.TEST}`);
 });
 
 app.listen(port, () => {
